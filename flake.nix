4761d159860e3931b845c6f16cd41d7d1c5c4aa3
acevif <acevif@gmail.com>
2026-02-12 09:14:31 +0900

    nix: add flake formatter using pkgs.nixfmt

--- flake.nix
+++ flake.nix
@@ -6,22 +6,30 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = inputs@{ self, flake-parts, ... }:
+  outputs =
+    inputs@{ self, flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "aarch64-darwin" "x86_64-darwin" ];
+      systems = [
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
 
-      perSystem = { pkgs, ... }: {
-        devShells.default = pkgs.mkShell {
-          packages = [
-            pkgs.swift
-            pkgs.swiftpm
-            pkgs.swiftpm2nix
-          ];
-        };
+      perSystem =
+        { pkgs, ... }:
+        {
+          formatter = pkgs.nixfmt;
 
-        packages.default = pkgs.callPackage ./nix/license-plist.nix {
-          src = self;
+          devShells.default = pkgs.mkShell {
+            packages = [
+              pkgs.swift
+              pkgs.swiftpm
+              pkgs.swiftpm2nix
+            ];
+          };
+
+          packages.default = pkgs.callPackage ./nix/license-plist.nix {
+            src = self;
+          };
         };
-      };
     };
 }
